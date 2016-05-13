4001d22a5a54db2e7b040af2fcef174cb6b63a44
NickyB <nickypoo.is@gmail.com>
2016-05-14 03:00:39 +1200

    added in dijkstra search to SiteMap... not yet tested!

---
+++ model/DijkstraNode.java
@@ -0,0 +1,38 @@
+package model;
+
+public class DijkstraNode {
+	int siteID;
+	int previousSiteID;//-1 if there is no previous site!
+	Route previousRoute;//null if there is no previous site!
+	double durationFromStart;
+
+	/** 
+	 * @param siteID - id of current site represented by this node
+	 * @param prevSiteID - id of site from which this node was reached (-1 if this is the start node)
+	 * @param route - Route used to get from previous site
+	 * @param durationFromStart - total duration to get from start site to this site during the Dijkstra search
+	 */
+	 public DijkstraNode(int siteID, int prevSiteID, Route prevRoute, double durationFromStart) {
+		this.siteID = siteID;
+		this.previousSiteID = prevSiteID;
+		this.durationFromStart = durationFromStart;
+		this.previousRoute = prevRoute;
+	}
+
+	public int getSiteID() {
+		return siteID;
+	}
+
+	public int getPrevSiteID() {
+		return previousSiteID;
+	}
+
+	public double getDurationFromStart() {
+		return durationFromStart;
+	}
+
+	public Route getPrevRoute() {
+		return previousRoute;
+	}
+
+}
--- storage/SiteMap.java
+++ storage/SiteMap.java
@@ -35,8 +35,7 @@ public class SiteMap {
 		return true;
 	}
 
-
-	//TODO: Not quite sure if we'll need this method here
+	// TODO: Not quite sure if we'll need this method here
 	/*
 	 * returns true if route successfully changed returns false if not
 	 * successful (eg the ID was invalid) [supporting soft failure]
@@ -70,8 +69,8 @@ public class SiteMap {
 	 * sites, carrier and duration are final, if they need to be changed a new
 	 * route should be made, and the old one discontinued.
 	 */
-	public boolean makeNewRoute(String origin, String destination, String company, int duration, Route.Mode mode, double custPriceWeight,
-			double custPriceVolume, double transPriceWeight, double transPriceVolume) {
+	public boolean makeNewRoute(String origin, String destination, String company, int duration, Route.Mode mode,
+			double custPriceWeight, double custPriceVolume, double transPriceWeight, double transPriceVolume) {
 		// check all input for values in correct format
 		if (duration <= 0 || custPriceWeight <= 0 || custPriceVolume <= 0 || transPriceWeight <= 0
 				|| transPriceVolume <= 0) {
@@ -85,47 +84,47 @@ public class SiteMap {
 			return false;
 		}
 		// find the site ids... make new sites if necessary
-		int originID = -1;//value indicates not yet found
-		int destinationID = -1;//value indicates not yet found
+		int originID = -1;// value indicates not yet found
+		int destinationID = -1;// value indicates not yet found
 		Set<Site> sites = getSites();
-		for(Site site : sites){
-			if (site.getLocation().equals(origin)){
-				originID=site.getID();
-			}
-			else if (site.getLocation().equals(destination)){
-				destinationID=site.getID();
+		for (Site site : sites) {
+			if (site.getLocation().equals(origin)) {
+				originID = site.getID();
+			} else if (site.getLocation().equals(destination)) {
+				destinationID = site.getID();
 			}
 		}
-		if (originID == -1){//origin site does not exist yet... make it!
-			originID = sites.size();//size is number of values already existing..
+		if (originID == -1) {// origin site does not exist yet... make it!
+			originID = sites.size();// size is number of values already
+									// existing..
 			// IDs made sequentially from 0 so size should be free!
-			Site originSite = new Site(originID,origin);
+			Site originSite = new Site(originID, origin);
 			addSite(originSite);
 		}
-		if (destinationID == -1){//destination site does not exist yet... make it!
-			destinationID = sites.size();//size is number of values already existing..
+		if (destinationID == -1) {// destination site does not exist yet... make
+									// it!
+			destinationID = sites.size();// size is number of values already
+											// existing..
 			// IDs made sequentially from 0 so size should be free!
-			Site destinationSite = new Site(destinationID,destination);
+			Site destinationSite = new Site(destinationID, destination);
 			addSite(destinationSite);
 		}
-		// check to see if route already exists - abort route creation if it does
+		// check to see if route already exists - abort route creation if it
+		// does
 		Set<Route> routes = getRoutes();
-		for(Route route : routes){
-			if (
-					route.getDestination() == destinationID
-					&& route.getOrigin() == originID
-					&& route.getCompany().equals(company)
-					&& route.getDuration() == duration
-					){
-				return false;//may need to do a price update instead
+		for (Route route : routes) {
+			if (route.getDestination() == destinationID && route.getOrigin() == originID
+					&& route.getCompany().equals(company) && route.getDuration() == duration) {
+				return false;// may need to do a price update instead
 			}
 		}
 		// find the next available ID (current length of routes list!)
-		int newRouteID = routes.size();//size is number of values already existing..
-				// IDs made sequentially from 0 so size should be free!
+		int newRouteID = routes.size();// size is number of values already
+										// existing..
+		// IDs made sequentially from 0 so size should be free!
 		// make route object
-		Route newRoute = new Route(newRouteID, originID, destinationID, company, duration, mode, true,
-				custPriceWeight, custPriceVolume, transPriceWeight, transPriceVolume );
+		Route newRoute = new Route(newRouteID, originID, destinationID, company, duration, mode, true, custPriceWeight,
+				custPriceVolume, transPriceWeight, transPriceVolume);
 		// add new route to the map
 		try {
 			addRoute(newRoute);
@@ -135,6 +134,35 @@ public class SiteMap {
 		}
 		return true;
 	}
+
+	// ======Dijkstra methods... for finding a compound route given from and to=======
+
+	/**
+	 * Uses the Dijkstra search method with duration as the value to minimise, to find
+	 * the shortest path from "from" to "to". Priority 1 searches may use SEA,
+	 * LAND and AIR routes whereas priority 2 searches may only use SEA and
+	 * LAND.
+	 * 
+	 * @param from:
+	 *            site that the compound route must start
+	 * @param to:
+	 *            site that the compound route must finish
+	 * @param priority:
+	 *            allowable values are 1 and 2
+	 * @return an ordered list of Route objects that compound to create a path
+	 *         from "from" to "to"
+	 */
+	public List<Route> findCompoundRoute(Site from, Site to, int priority) {
+		if (priority == 1) {
+			return new P1DijkstraSearch(from,to,siteToRoutes).findShortestRoute(); 
+			}
+		if (priority == 2) {
+			return new P2DijkstraSearch(from,to,siteToRoutes).findShortestRoute(); 
+		}
+		// priority was not recognised!
+		return null;
+	}
+
 
 	// ========== internal helper methods for dealing with the map
 	// structure=======
