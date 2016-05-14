967cd30300d5f92d974c540f74ff0ec527569d9a
xuanshenbo <xuanshenbo@hotmail.com>
2016-05-14 17:21:56 +1200

    add close the frame using the X button functionality

--- view/KPSmartFrame.java
+++ view/KPSmartFrame.java
@@ -22,36 +22,37 @@ public final class KPSmartFrame extends JFrame {
 	private static final int FRAME_WIDTH = 1600;
 	private static final int FRAME_HEIGHT = 900;
 
-//	public KPSmartFrame() {
-//		super("KPSmart");
-//		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-//		setResizable(true);
-//
-//		setLayout(new BorderLayout());
-//
-//		canvas = new KPSmartCanvas(this);
-//		add(canvas);
-//
-//		createMenu();
-//		pack();
-//
-//		setVisible(true);
-//
-//		this.addWindowListener(new WindowAdapter() {
-//			@Override
-//			public void windowClosing(WindowEvent we) {
-//				String[] options = { "YES", "Cancel" };
-//				JPanel panel = new JPanel();
-//				JLabel label = new JLabel("Are you sure you want to quit the system?");
-//				panel.add(label);
-//				int selectedOption = JOptionPane.showOptionDialog(null, panel, "Warning!!!", JOptionPane.DEFAULT_OPTION,
-//						JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-//				if (selectedOption == 0) {
-//					System.exit(0);
-//				}
-//			}
-//		});
-//	}
+	// public KPSmartFrame() {
+	// super("KPSmart");
+	// setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	// setResizable(true);
+	//
+	// setLayout(new BorderLayout());
+	//
+	// canvas = new KPSmartCanvas(this);
+	// add(canvas);
+	//
+	// createMenu();
+	// pack();
+	//
+	// setVisible(true);
+	//
+	// this.addWindowListener(new WindowAdapter() {
+	// @Override
+	// public void windowClosing(WindowEvent we) {
+	// String[] options = { "YES", "Cancel" };
+	// JPanel panel = new JPanel();
+	// JLabel label = new JLabel("Are you sure you want to quit the system?");
+	// panel.add(label);
+	// int selectedOption = JOptionPane.showOptionDialog(null, panel,
+	// "Warning!!!", JOptionPane.DEFAULT_OPTION,
+	// JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+	// if (selectedOption == 0) {
+	// System.exit(0);
+	// }
+	// }
+	// });
+	// }
 
 	public KPSmartFrame(KeyAction keyAction, MouseAction mouseAction, ViewActionListener viewActionListener) {
 
@@ -69,6 +70,21 @@ public final class KPSmartFrame extends JFrame {
 		pack();
 
 		setVisible(true);
+
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent we) {
+				String[] options = { "YES", "Cancel" };
+				JPanel panel = new JPanel();
+				JLabel label = new JLabel("Are you sure you want to quit the system?");
+				panel.add(label);
+				int selectedOption = JOptionPane.showOptionDialog(null, panel, "Warning!!!", JOptionPane.DEFAULT_OPTION,
+						JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+				if (selectedOption == 0) {
+					System.exit(0);
+				}
+			}
+		});
 	}
 
 	public void removeOldCanvas() {
