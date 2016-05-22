107d3679d16755d067a08a1dc19ac4fec33cd900
joelzhuang <joely.huang@gmail.com>
2016-05-22 19:16:33 +1200

    done methods

--- model/EventProcessor.java
+++ model/EventProcessor.java
@@ -3,7 +3,11 @@ package model;
 import java.time.LocalDateTime;
 
 import model.events.BusinessEvent;
+import model.events.CustPriceChangeEvent;
 import model.events.MailProcessEvent;
+import model.events.RouteAdditionEvent;
+import model.events.RouteDiscEvent;
+import model.events.TransportCostChangeEvent;
 import storage.DataStore;
 
 /**
@@ -29,17 +33,80 @@ public class EventProcessor {
 	// if changes to the carrier, mode, or duration are needed, the route should
 	// be discontinued,
 	// and a new one made
+	
+	/**
+	 * 
+	 * @param routeID --> in constructor, there is origin and destination???
+	 * @param need a priority for constructor as well?
+	 */
 	public void changeCustomerPrice(int routeID, double newCustPriceWeight, double newCustPriceVolume, int processorStaffID) {
-		System.out.println(debuggingString + debuggingInt + 1);
+		LocalDateTime now = LocalDateTime.now();
+		int day = now.getDayOfMonth();
+		int month = now.getMonthValue();
+		int year = now.getYear();
+		int time = now.getHour()*10 + now.getMinute();
+		String employee = db.getEmployees().getEmployeeFromID(processorStaffID).getName();
+		String origin; //need something here
+		String destination; //need something here
+		Priority priority;	//need something here
+		BusinessEvent ccpBusinessEvent = new CustPriceChangeEvent(day, month, year, time, employee, origin, destination,
+				priority, newCustPriceWeight, newCustPriceVolume);
+		db.addEvent(ccpBusinessEvent);
+		
+		//System.out.println(debuggingString + debuggingInt + 1);
 	};
 
 	public void changeTransportCost(int routeID, double newTransCostWeight, double newTransCostVolume, int processorStaffID) {
-		System.out.println(debuggingString + debuggingInt + 2);
+		LocalDateTime now = LocalDateTime.now();
+		int day = now.getDayOfMonth();
+		int month = now.getMonthValue();
+		int year = now.getYear();
+		int time = now.getHour()*10 + now.getMinute();
+		String employee = db.getEmployees().getEmployeeFromID(processorStaffID).getName();
+		String origin;
+		String destination;
+		String company;
+		String type;
+		String departureDay;
+		int frequency;
+		int duration;
+		BusinessEvent ctcBusinessEvent = new TransportCostChangeEvent(day, month, year, time, employee, origin, destination,
+				company, type, newTransCostWeight, newTransCostVolume, departureDay, frequency, duration);
+		db.addEvent(ctcBusinessEvent);
+		//System.out.println(debuggingString + debuggingInt + 2);
 	};
 
+	/**
+	 * 
+	 * @param toSiteID
+	 * @param fromSiteID
+	 * @param carrier
+	 * @param duration
+	 * @param custPriceWeight
+	 * @param custPriceVolume
+	 * @param transPriceWeight ??? Constructor does not have this field
+	 * @param transPriceVolume ??? Constructor does not have this field
+	 * @param processorStaffID
+	 */
 	public void addRoute(int toSiteID, int fromSiteID, String carrier, double duration, double custPriceWeight,
 			double custPriceVolume, double transPriceWeight, double transPriceVolume, int processorStaffID) {
-		System.out.println(debuggingString + debuggingInt + 3);
+		LocalDateTime now = LocalDateTime.now();
+		int day = now.getDayOfMonth();
+		int month = now.getMonthValue();
+		int year = now.getYear();
+		int time = now.getHour()*10 + now.getMinute();
+		String employee = db.getEmployees().getEmployeeFromID(processorStaffID).getName();
+		String origin= db.getSiteMap().getSitefromID(fromSiteID);;
+		String destination = db.getSiteMap().getSitefromID(toSiteID);;
+		String company = carrier;
+		String type;
+		String departureDay;
+		int frequency;
+		BusinessEvent arBusinessEvent = new RouteAdditionEvent(day, month, year, time, employee, origin, destination,
+				company, type, custPriceWeight, custPriceVolume, departureDay, frequency, duration);
+		db.addEvent(arBusinessEvent);
+		
+		//System.out.println(debuggingString + debuggingInt + 3);
 	};
 
 	// 1
@@ -60,9 +127,22 @@ public class EventProcessor {
 				weight, volume, priority, revenue, expenditure, deliveryTime);
 		db.addEvent(pmBusinessEvent);
 	}
-
+	
 	public static void disconRoute(int routeID) {
-		System.out.println(debuggingString + debuggingInt + 5);
+		LocalDateTime now = LocalDateTime.now();
+		int day = now.getDayOfMonth();
+		int month = now.getMonthValue();
+		int year = now.getYear();
+		int time = now.getHour()*10 + now.getMinute();
+		String employee = db.getEmployees().getEmployeeFromID(processorStaffID).getName();
+		String origin;
+		String destination;
+		String company;
+		String type;
+		BusinessEvent drBusinessEvent = new RouteDiscEvent(day, month, year, time, employee, origin, destination,
+				company, type);
+		db.addEvent(drBusinessEvent);
+		//System.out.println(debuggingString + debuggingInt + 5);
 	}
 
 	public static void pushEvent() {
