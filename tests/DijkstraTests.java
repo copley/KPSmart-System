3794730f2c98ee40da703ad19753df5f03791ba8
NickyB <nickypoo.is@gmail.com>
2016-06-09 15:02:36 +1200

    Fixed up the dijkstra tests, were not passing because of changes to the sitemap structure that were not reflected in the dummySitemap!

--- tests/DijkstraTests.java
+++ tests/DijkstraTests.java
@@ -18,10 +18,10 @@ public class DijkstraTests {
 	@Test
 	public void testAirDirect() {
 		List<Integer> correct = new LinkedList<Integer>();
-		correct.add(2);
-		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("A"),
-				testMap.getSiteIDfromLocation("C"), Priority.DOMESTIC_AIR);
-		assertTrue("The compound route should be direct from A to C", compoundRoute.equals(correct));
+		correct.add(3);
+		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("Auckland"),
+				testMap.getSiteIDfromLocation("Christchurch"), Priority.DOMESTIC_AIR);
+		assertTrue("The compound route should be direct from Auckland to Christchurch", compoundRoute.equals(correct));
 	}
 
 	@Test
@@ -29,40 +29,42 @@ public class DijkstraTests {
 		// should end up having to use the non-air route as the air route is
 		// discontinued
 		List<Integer> correct = new LinkedList<Integer>();
-		correct.add(1);
-		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("A"),
-				testMap.getSiteIDfromLocation("B"), Priority.DOMESTIC_AIR);
-		assertTrue("The compound route should be land from A to B", compoundRoute.equals(correct));
+		correct.add(2);
+		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("Auckland"),
+				testMap.getSiteIDfromLocation("Bluff"), Priority.DOMESTIC_AIR);
+		assertTrue("The compound route should be land from Auckland to Bluff", compoundRoute.equals(correct));
 	}
 
 	@Test
 	public void test2Airs() {
 		// should end up finding 2 air routes one after the other
 		List<Integer> correct = new LinkedList<Integer>();
-		correct.add(2);
-		correct.add(4);
-		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("A"),
-				testMap.getSiteIDfromLocation("D"), Priority.DOMESTIC_AIR);
-		System.out.println(compoundRoute);
-		assertTrue("The compound route should be air A to C to D", compoundRoute.equals(correct));
+		correct.add(3);
+		correct.add(5);
+		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("Auckland"),
+				testMap.getSiteIDfromLocation("Dunedin"), Priority.DOMESTIC_AIR);
+		if(compoundRoute == null){
+			System.out.println("test2airs was null!");
+		}
+		assertTrue("The compound route should be air Auckland to Christchurch to Dundedin", compoundRoute.equals(correct));
 	}
 
 	@Test
 	public void test2Standards() {
 		// should end up finding 2 standard routes one after the other
 		List<Integer> correct = new LinkedList<Integer>();
-		correct.add(1);
-		correct.add(3);
-		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("A"),
-				testMap.getSiteIDfromLocation("C"), Priority.DOMESTIC_STANDARD);
-		assertTrue("The compound route should be road A to B then ship to C", compoundRoute.equals(correct));
+		correct.add(2);
+		correct.add(4);
+		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("Auckland"),
+				testMap.getSiteIDfromLocation("Christchurch"), Priority.DOMESTIC_STANDARD);
+		assertTrue("The compound route should be road Auckland to Bluff then ship to Christchurch", compoundRoute.equals(correct));
 	}
 
 	@Test
 	public void testNoStandard() {
 		// should end up not finding a standard route
-		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("A"),
-				testMap.getSiteIDfromLocation("D"), Priority.DOMESTIC_STANDARD);
+		List<Integer> compoundRoute = testMap.findCompoundRoute(testMap.getSiteIDfromLocation("Auckland"),
+				testMap.getSiteIDfromLocation("Dunedin"), Priority.DOMESTIC_STANDARD);
 		assertTrue("The compound route should be null", compoundRoute == null);
 	}
 
@@ -71,29 +73,26 @@ public class DijkstraTests {
 		SiteMap siteMap = new SiteMap();
 
 		// make the routes we need
-		// route 0
-		siteMap.addNewRoute("A", "B", "Air Dingus", Type.AIR, 20, 100, 100, 100, 100);
-		siteMap.discontinueRoute(0);
 		// route 1
-		siteMap.addNewRoute("A", "B", "Trucky McTruck", Type.LAND, 120, 10, 10, 10, 10);
+		siteMap.addNewRoute("Auckland", "Bluff", "Air Dingus", Type.AIR, 20, 100, 100, 100, 100);
+		siteMap.discontinueRoute(1);
 		// route 2
-		siteMap.addNewRoute("A", "C", "Air Dingus", Type.AIR, 20,100, 100, 100, 100);
+		siteMap.addNewRoute("Auckland", "Bluff", "Trucky McTruck", Type.LAND, 120, 10, 10, 10, 10);
 		// route 3
-		siteMap.addNewRoute("B", "C", "HMS BoatFace", Type.SEA, 240, 1, 1, 1, 1);
+		siteMap.addNewRoute("Auckland", "Christchurch", "Air Dingus", Type.AIR, 20,100, 100, 100, 100);
 		// route 4
-		siteMap.addNewRoute("C", "D", "Air Dingus", Type.AIR, 20, 100, 100, 100, 100);
+		siteMap.addNewRoute("Bluff", "Christchurch", "HMS BoatFace", Type.SEA, 240, 1, 1, 1, 1);
 		// route 5
-		siteMap.addNewRoute("C", "D", "Trucky McTruck", Type.LAND, 120, 10, 10, 10, 10);
-		siteMap.discontinueRoute(5);
-
-		System.out.println("Test Map Routes:");
-		System.out.println(siteMap.getRoutes().toString());
-		System.out.println("Test Map Sites: ");
-		System.out.println(siteMap.getSites().toString());
-		System.out.println("Test Map Site to Routes entries: ");
-		for (Site site : siteMap.getSites()) {
-			System.out.println(site.getLocation() + " routes on: " + siteMap.getRoutesOn(site.getID()));
-		}
+		siteMap.addNewRoute("Christchurch", "Dunedin", "Air Dingus", Type.AIR, 20, 100, 100, 100, 100);
+		
+//		System.out.println("Test Map Routes:");
+//		System.out.println(siteMap.getRoutes().toString());
+//		System.out.println("Test Map Sites: ");
+//		System.out.println(siteMap.getSites().toString());
+//		System.out.println("Test Map Site to Routes entries: ");
+//		for (Site site : siteMap.getSites()) {
+//			System.out.println(site.getLocation() + " routes on: " + siteMap.getRoutesOn(site.getID()));
+//		}
 
 		return siteMap;
 	}
